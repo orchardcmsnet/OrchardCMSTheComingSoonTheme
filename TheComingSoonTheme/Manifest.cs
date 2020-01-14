549d6de6e0f6b1e7587209bc007cf32e1971a52f
orchardcmsnet <blazor@orchardcms.net>
2020-01-13 21:29:09 -0700

    Added The OrchardCMS Coming Soon Theme

---
+++ TheComingSoonTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The OrchardCMS.net Coming Soon Theme",
+    Author = "OrchardCMS.net",
+    Website = "https://OrchardCMS.net",
+    Version = "2.0.0",
+    Description = "OrchardCMS.net Landing page for a project that is under construction"
+)]
